7d0747f0644f53316e8df13b650d243f4c6928ad
lupak <david.lupak@htw-berlin.de>
2021-07-23 23:36:59 +0200

    planned graphic drawer, switched zoom setting

--- src/graphicsviewdrawer.cpp
+++ src/graphicsviewdrawer.cpp
@@ -13,11 +13,11 @@ void GraphicsViewDrawer::SetupScene(QGraphicsView *view_to_setup)
 
     scene->addText("Hello World!");
 
-    scene->addRect(10000, 0, 80, 100, outlinePen, blueBrush);
+    scene->addRect(300, 0, 80, 100, outlinePen, blueBrush);
+    scene->addRect(200, 0, 80, 100, outlinePen, blueBrush);
+
+
     scene->addRect(1000, 0, 80, 100, outlinePen, blueBrush);
-
-
-    scene->addRect(2000, 0, 80, 100, outlinePen, blueBrush);
     scene->setSceneRect(0,0,0,0);
     view_to_setup->setDragMode(QGraphicsView::DragMode::ScrollHandDrag);
     view_to_setup->setResizeAnchor(QGraphicsView::ViewportAnchor::AnchorUnderMouse);
--- src/mousezoomhandler.h
+++ src/mousezoomhandler.h
@@ -18,8 +18,8 @@ private:
     inline static double current_zoom_x = 1;
     inline static qreal current_zoom_y = 1;
 
-    inline static qreal zoom_speed_x = 0;
-    inline static qreal zoom_speed_y = 0.1;
+    inline static qreal zoom_speed_x = 0.1;
+    inline static qreal zoom_speed_y = 0;
 
 
     inline static QGraphicsView *handled_view;
